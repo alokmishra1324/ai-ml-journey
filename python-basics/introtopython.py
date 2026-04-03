9735fd7a0199e4ecc9d57b0194f35a3d0b7c6c5f
Alok Kumar Mishra <alokkumar_mishra@epam.com>
2026-04-04 01:23:59 +0530

    Intro to python basics

---
+++ python-basics/introtopython.py
@@ -0,0 +1,45 @@
+## this is single line comment
+'''
+this is multi line comment
+
+'''
+
+print('Hello World!')
+
+name = "Alok"
+Name = "Mishra"
+
+print(name)
+print(Name)
+
+##Indentation
+age=32
+if age>30:
+    print(age)
+    
+print(age)    
+
+## Line Continuation
+total = 1+2+3+4+6\
++7+8
+
+print(total)
+
+##Multiple statements
+
+x=20;y=8;z=x+y
+print(z)
+
+
+print(type(age))
+print(type(name))
+
+##code examples of indentation
+if True:
+    print("This will print")
+    if False:
+        print("This will nor print")
+    print("THIS will print")
+print("This also print")    
+
+
