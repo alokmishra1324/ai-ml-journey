91fb7d3c627b3c39ae25833fc40ee9830ff4b7f8
Alok Mishra <alokbuntymishra@gmail.com>
2026-04-08 20:26:30 +0530

    operators in python

---
+++ python-basics/operators.py
@@ -0,0 +1,67 @@
+a =21
+b=5
+
+add_res = a+b
+sub_res = a-b
+div_res = a/b
+multi_res = a*b
+floor_div_res = a//b
+modulus_res =  a%b
+exponential_res = a**b
+
+print(add_res)
+print(sub_res)
+print(div_res)
+print(multi_res)
+print(floor_div_res)
+print(modulus_res)
+print(exponential_res)
+
+## Comparison Operators
+## ==  Equal to
+a=10
+b=11
+
+print(a==b)
+
+str1= "alok"
+str2 = "alok"
+
+print(str1 == str2)
+
+## != Not wqual to
+
+print(str1 != str2)
+
+## > , < 
+
+print(a>b)
+
+## <= , >=
+print(a<=b)
+
+## Logical Operator
+## AND , OR , NOT
+
+x= False
+y= False
+res = x and y
+print(res)
+
+res2 = x or y
+print(res2)
+
+res3 = not y
+print(res3)
+
+
+num1 = -98
+if(num1 > 0):
+    print("Positive")
+if(num1 < 0):
+    print("Negative")
+if(num1 == 0):
+    print("Zero")
+
+
+
