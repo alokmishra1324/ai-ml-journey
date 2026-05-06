52f8cf9b40c70d3ba299630f6c542407426017c0
Alok Mishra <alokbuntymishra@gmail.com>
2026-05-06 22:38:34 +0530

    Multithreading in python

---
+++ multithreading_multiprocessing/multithreading.py
@@ -0,0 +1,38 @@
+## Multithreading
+## When to use Multi Threading
+
+##I/O- bound tasks : Tasks that spend more time waiting for I/O operations 
+## Concurrent execution
+
+import threading
+import time
+
+def print_num():
+    for i in range(5):
+        time.sleep(2)
+        print(f"Number:{i}")
+
+def print_letter():
+    for letter in "abcde":
+        time.sleep(2)
+        print(f"Letter :{letter}")
+
+##create 2 threads
+
+t1 = threading.Thread(target=print_num)
+t2 = threading.Thread(target = print_letter)
+
+t = time.time()
+## start the thread
+t1.start()
+t2.start()
+
+## Wait for the threads to complete
+t1.join()
+t2.join()
+
+# print_num()
+# print_letter()
+finished_time = time.time()-t 
+print(finished_time)
+
