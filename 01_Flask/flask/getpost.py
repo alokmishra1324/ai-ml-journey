c3480b7e6fa009bc0e4d7bbb6b7668d25392ee20
Alok Mishra <alokbuntymishra@gmail.com>
2026-05-10 21:26:58 +0530

    GET POST using flask

---
+++ 01_Flask/flask/getpost.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template , request
+
+'''
+It creates an instance of the Flask class , 
+which will be your WSGI application
+'''
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><h1>Welcome to the course</h1></html>"
+
+@app.route("/index" , methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route('/form' , methods=['GET' , 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"Hello {name}"
+    return render_template('form.html')
+
+@app.route('/submit' , methods=['GET' , 'POST'])
+def sumbit():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"Hello {name}"
+    return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
